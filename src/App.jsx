4cb01925409ce1206cb7eca0bc0f7fd27b9131f8
idaoskooei <idaoskooei@gmail.com>
2024-07-30 17:18:17 -0400

    add basic structure of the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.scss'
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header/Header';
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+    <Header />
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
