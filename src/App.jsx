c2635b47d8a5d76d6ac408fb952a541a5d08f0c2
Kowsiya Srikantharajah <kowsiya.sri@gmail.com>
2024-07-31 19:33:52 -0400

    Add api call to retrieve data for single warehouse and update warehousedetails page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
 import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx";
-// import EditWarehousePage from "./pages/EditWarehousePage/EditWarehousePage";
-// import InventoryPage from "./pages/InventoryPage/InventoryPage";
-// import InventoryDetailsPage from "./pages/InventoryDetailsPage/InventoryDetailsPage";
-// import EditInventoryPage from "./pages/EditInventoryPage/EditInventoryPage";
-// import AddInventoryPage from "./pages/AddInventoryPage/AddInventoryPage";
-// import AddWarehousePage from "./pages/AddWarehousePage/AddWarehousePage";
+import EditWarehousePage from "./pages/EditWarehousePage/EditWarehousePage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import InventoryDetailsPage from "./pages/InventoryDetailsPage/InventoryDetailsPage";
+import EditInventoryPage from "./pages/EditInventoryPage/EditInventoryPage";
+import AddInventoryPage from "./pages/AddInventoryPage/AddInventoryPage";
+import AddWarehousePage from "./pages/AddWarehousePage/AddWarehousePage";
 import Header from "./components/Header/Header";
 import { Navigate } from "react-router-dom";
 
@@ -24,12 +24,27 @@ function App() {
                         path="/warehouses/:id"
                         element={<WarehouseDetailsPage />}
                     />
-                    {/* <Route path="/warehouses/:id/edit" element={<EditWarehousePage />} />
-          <Route path="/inventory" element={<InventoryPage />} />
-          <Route path="/inventory/:id" element={<InventoryDetailsPage />} />
-          <Route path="/inventory/:id/edit" element={<EditInventoryPage />} />
-          <Route path="/add-inventory" element={<AddInventoryPage />} />
-          <Route path="/add-warehouse" element={<AddWarehousePage />} /> */}
+                    <Route
+                        path="/warehouses/:id/edit"
+                        element={<EditWarehousePage />}
+                    />
+                    <Route path="/inventory" element={<InventoryPage />} />
+                    <Route
+                        path="/inventory/:id"
+                        element={<InventoryDetailsPage />}
+                    />
+                    <Route
+                        path="/inventory/:id/edit"
+                        element={<EditInventoryPage />}
+                    />
+                    <Route
+                        path="/add-inventory"
+                        element={<AddInventoryPage />}
+                    />
+                    <Route
+                        path="/add-warehouse"
+                        element={<AddWarehousePage />}
+                    />
                 </Routes>
             </div>
         </Router>
