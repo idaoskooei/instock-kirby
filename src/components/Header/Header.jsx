673fec5affbb38787947a90a03c64301b9185a31
idaoskooei <idaoskooei@gmail.com>
2024-07-31 11:01:46 -0400

    header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,23 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Logo from "../../assets/Logo/InStock-Logo_1x.png";
 import "./Header.scss";
 
-const Header = () => {
-  return (
-    <header className="header">
-      <div className="header__container">
-        <Link to="/" className="header__logo">
-          <img src={Logo} alt="logo" />
-        </Link>
-        <div className="header__list">
-          <NavLink
-            to="/warehouse"
-            className="header__list-item"
-            activeClassName="header__list-item--active"
-          >
-            Warehouses
-          </NavLink>
-          <NavLink
-            to="/inventory"
-            className="header__list-item"
-            activeClassName="header__list-item--active"
-          >
-            Inventory
-          </NavLink>
-        </div>
-      </div>
-    </header>
-  );
+const Header = () =>  {
+    return (
+        <header className="header">
+          <section className="header__container">
+               <Link to= "/" className="header__logo">
+                   <img src={Logo}  alt="logo" />
+                </Link>
+                <div className="header__list">
+                    <NavLink to="/warehouse" className="header__list-item" >Warehouse</NavLink>
+                    <NavLink to="/inventory"  className="header__list-item">Inventory</NavLink>
+                </div>
+            </section>
+        </header>
+    );
 }
 
 export default Header;
