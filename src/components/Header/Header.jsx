bfebac6c93ba8c841117f5bc40360a5d79b6281c
idaoskooei <idaoskooei@gmail.com>
2024-07-31 11:01:07 -0400

    add header component but buttons need fixing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,34 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { NavLink } from "react-router-dom";
-import Logo from "../../assets/Logo/InStock-Logo_1x.png"
-import "../Header/Header.scss";
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../../assets/Logo/InStock-Logo_1x.png";
+import "./Header.scss";
 
-class Header extends React.Component {
-  render() {
-    return (
-        <header className="header">
-          <section className="header__container">
-               <Link to= "/" className="header__logo">
-                   <img src={Logo}  alt="logo" />
-                </Link>
-                <div className="header__list">
-                    <NavLink to="/warehouse" className="header__list-item" >Warehouse</NavLink>
-                    <NavLink to="/inventory"  className="header__list-item">Inventory</NavLink>
-                </div>
-            </section>
-        </header>
-    );
-  }
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <Link to="/" className="header__logo">
+          <img src={Logo} alt="logo" />
+        </Link>
+        <div className="header__list">
+          <NavLink
+            to="/warehouse"
+            className="header__list-item"
+            activeClassName="header__list-item--active"
+          >
+            Warehouses
+          </NavLink>
+          <NavLink
+            to="/inventory"
+            className="header__list-item"
+            activeClassName="header__list-item--active"
+          >
+            Inventory
+          </NavLink>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
