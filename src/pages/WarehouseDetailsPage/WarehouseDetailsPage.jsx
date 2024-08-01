b311b827c36140cc2d6b325692694c3dec6f9e72
Kowsiya Srikantharajah <kowsiya.sri@gmail.com>
2024-08-01 11:22:38 -0400

    Update warehouse details page by adding details headers

--- src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
@@ -3,38 +3,42 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import InventoryList from "../../components/InventoryList/InventoryList";
 import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
+import DetailsPageHeader from "../../components/DetailsPageHeader/DetailsPageHeader";
 
 const WarehouseDetailsPage = () => {
-  // Set variables
-  const [warehouse, setWarehouse] = useState(null);
-  const { id } = useParams();
+    // Set variables
+    const [warehouse, setWarehouse] = useState(null);
+    const { id } = useParams();
 
-  // Get the warehouse details from the API
-  const API_BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+    // Get the warehouse details from the API
+    const API_BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
-  const getSelectedWarehouse = async (id) => {
-    try {
-      let response = await axios.get(`${API_BASE_URL}/api/warehouses/${id}`);
-      setWarehouse(response.data);
-    } catch (error) {
-      console.error("Error retrieving the warehouse.", error);
+    const getSelectedWarehouse = async (id) => {
+        try {
+            let response = await axios.get(
+                `${API_BASE_URL}/api/warehouses/${id}`
+            );
+            setWarehouse(response.data);
+        } catch (error) {
+            console.error("Error retrieving the warehouse.", error);
+        }
+    };
+
+    useEffect(() => {
+        getSelectedWarehouse(id);
+    }, [id]);
+
+    if (!warehouse) {
+        return <>Loading data...</>;
     }
-  };
 
-  useEffect(() => {
-    getSelectedWarehouse(id);
-  }, [id]);
-
-  if (!warehouse) {
-    return <>Loading data...</>;
-  }
-
-  return (
-    <div>
-      <WarehouseDetails warehouse={warehouse} />
-      <InventoryList warehouseId={id} showWarehouseName={false} />
-    </div>
-  );
+    return (
+        <div>
+            <DetailsPageHeader name={warehouse.warehouse_name} hasEdit={true} />
+            <WarehouseDetails warehouse={warehouse} />
+            <InventoryList warehouseId={id} showWarehouseName={false} />
+        </div>
+    );
 };
 
 export default WarehouseDetailsPage;
