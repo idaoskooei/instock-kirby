25fe9d3724e95c7506e5a6bdd4e8b86a1cdfc82d
Melody <melodytekle@gmail.com>
2024-08-01 08:35:20 -0400

    created header for both warehouse and inventory page with a search bar component

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./SearchBar.css";
+
+const SearchBar = ({ placeholder }) => {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder={placeholder} readOnly />
+      <img
+        src="../../icons/search-24px.svg"
+        alt="Search Icon"
+        className="search-icon"
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,10 +1,18 @@
 import React from "react";
-import InventoryList from "../components/InventoryList";
+import InventoryList from "../../components/InventoryList/InventoryList";
+import MainPageHeader from "../../components/MainPageHeader/MainPageHeader";
+import Footer from "../../components/Footer/Footer";
 
 const InventoryPage = () => {
   return (
     <div>
+      <MainPageHeader
+        title="Inventory"
+        addButtonText="+ Add New Item"
+        addButtonLink="/add-inventory"
+      />
       <InventoryList />
+      <Footer />
     </div>
   );
 };
