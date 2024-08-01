f3a5908f39fb40a3858c156eec946945f268211d
oolongjiawei <oolongjiawei@gmail.com>
2024-08-01 13:36:01 -0400

    style the component MainPageHerder, style the wareHouseList

--- src/components/MainPageHeader/MainPageHeader.jsx
+++ src/components/MainPageHeader/MainPageHeader.jsx
@@ -1,14 +1,15 @@
+import './MainPageHeader.scss';
 import React from "react";
 import { Link } from "react-router-dom";
-import SearchBar from "./SearchBar";
+import SearchBar from "../SearchBar/SearchBar.jsx";
 
 const MainPageHeader = ({ title, addButtonText, addButtonLink }) => {
   return (
     <div className="main-page-header">
-      <h1>{title}</h1>
+      <h1 className='main-page-title'>{title}</h1>
       <SearchBar placeholder="Search..." />
       <Link to={addButtonLink}>
-        <button>{addButtonText}</button>
+        <button className='btn'>{addButtonText}</button>
       </Link>
     </div>
   );
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import "./SearchBar.css";
+import "./SearchBar.scss";
 
 const SearchBar = ({ placeholder }) => {
   return (
     <div className="search-bar">
-      <input type="text" placeholder={placeholder} readOnly />
-      <img
-        src="../../icons/search-24px.svg"
-        alt="Search Icon"
-        className="search-icon"
-      />
+      <input className="search-input" type="text" placeholder={placeholder} readOnly />
     </div>
   );
 };
