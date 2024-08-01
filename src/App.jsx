8b25a3f49f14cea17d24ec7698fc2a390416c3a7
oolongjiawei <oolongjiawei@gmail.com>
2024-08-01 14:46:29 -0400

    app.jsx fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import "./App.scss";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WarehousePage from "./pages/WarehousePage/WarehousePage";
-// import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx";
-// import EditWarehousePage from "./pages/EditWarehousePage/EditWarehousePage";
-// import InventoryPage from "./pages/InventoryPage/InventoryPage";
-// import InventoryDetailsPage from "./pages/InventoryDetailsPage/InventoryDetailsPage";
-// import EditInventoryPage from "./pages/EditInventoryPage/EditInventoryPage";
-// import AddInventoryPage from "./pages/AddInventoryPage/AddInventoryPage";
-// import AddWarehousePage from "./pages/AddWarehousePage/AddWarehousePage";
+import WarehouseDetailsPage from "./pages/WarehouseDetailsPage/WarehouseDetailsPage";
+import EditWarehousePage from "./pages/EditWarehousePage/EditWarehousePage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
+import InventoryDetailsPage from "./pages/InventoryDetailsPage/InventoryDetailsPage";
+import EditInventoryPage from "./pages/EditInventoryPage/EditInventoryPage";
+import AddInventoryPage from "./pages/AddInventoryPage/AddInventoryPage";
+import AddWarehousePage from "./pages/AddWarehousePage/AddWarehousePage";
 import Header from "./components/Header/Header";
 import { Navigate } from "react-router-dom";
 
@@ -20,7 +20,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Navigate to="/warehouses" />} />
                     <Route path="/warehouses" element={<WarehousePage />} />
-                    {/* <Route
+                    <Route
                         path="/warehouses/:id"
                         element={<WarehouseDetailsPage />}
                     />
@@ -44,7 +44,7 @@ function App() {
                     <Route
                         path="/add-warehouse"
                         element={<AddWarehousePage />}
-                    /> */}
+                    />
                 </Routes>
             </div>
         </Router>
